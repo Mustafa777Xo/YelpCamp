ac1ed5b863e0a40b140b183459b4cf7dbb391eb8
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-21 21:20:54 +0300

    added tailwind and react-router-dom and configure the router

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1 className="text-amber-500">Landing Page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page NotFound</h1>
+    </div>
+  )
+}
+
+export default NotFound
