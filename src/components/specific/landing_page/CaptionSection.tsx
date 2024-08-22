044ed0110b88bd778fadc150a382c7ea97ff121e
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-22 21:42:20 +0300

    landing page finished

--- src/components/specific/landing_page/CaptionSection.tsx
+++ src/components/specific/landing_page/CaptionSection.tsx
@@ -1,6 +1,7 @@
 import BaseButton from "../../common/BaseButton";
 import Logo from "../../../assets/images/Logo.svg";
 import CampFeature from "./CampFeature";
+import PartneredWith from "./PartneredWith";
 
 const features = [
   "Add your own camp suggestions.",
@@ -9,12 +10,12 @@ const features = [
 ];
 const CaptionSection = () => {
   return (
-    <div className="p-8 lg:h-screen lg:pt-5 lg:px-24">
-      <img src={Logo} alt="" className="lg:mb-60 mb-10" />
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">
+    <div className="px-12 lg:h-screen lg:pt-5 lg:px-24">
+      <img src={Logo} alt="" className="lg:mb-60 mb-8 mt-2" />
+      <h1 className="text-4xl lg:text-6xl font-bold mb-4">
         Explore the best camps on Earth.
       </h1>
-      <p className="text-lg lg:text-xl mb-6 text-gray-500">
+      <p className="text-sm md:text-md lg:text-xl mb-6 text-gray-500">
         YelpCamp is a curated list of the best camping spots on Earth.
         Unfiltered and unbiased reviews.
       </p>
@@ -31,6 +32,7 @@ const CaptionSection = () => {
         View Campgrounds
       "
       />
+      <PartneredWith />
     </div>
   );
 };
--- src/components/specific/landing_page/PartneredWith.tsx
+++ src/components/specific/landing_page/PartneredWith.tsx
@@ -1,10 +1,17 @@
-
+import plumGuideLogo from "../../../assets/images/Plum Guide.svg";
+import bookingsLogo from "../../../assets/images/booking.svg";
+import airbnbLogo from "../../../assets/images/airbnb.svg";
 const PartneredWith = () => {
   return (
-    <div>
-      part
+    <div className="container mt-10">
+      <h5>Partnered With:</h5>
+      <div className="md:flex mt-2">
+        <img src={bookingsLogo} alt="" />
+        <img src={plumGuideLogo} alt="" />
+        <img src={airbnbLogo} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PartneredWith
+export default PartneredWith;
