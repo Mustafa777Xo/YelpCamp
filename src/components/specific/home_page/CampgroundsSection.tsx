0be7cee382ae1e387f2785bb1f85d1294aed54de
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-27 21:01:56 +0300

    link home camp to the camp details page

--- src/components/specific/home_page/CampgroundsSection.tsx
+++ src/components/specific/home_page/CampgroundsSection.tsx
@@ -5,38 +5,45 @@ import onayBeach from "@/assets/images/camps/Onay_Beach.jpg";
 import sevenSistersWaterfall from "@/assets/images/camps/Seven_Sisters_Waterfall.jpg";
 import latikReverside from "@/assets/images/camps/Latik_Riverside.jpg";
 import buloySprings from "@/assets/images/camps/Buloy_Springs.jpg";
+import { Link } from "react-router-dom";
 const CampgroundsSection = () => {
   const cards = [
     {
+      id: 1,
       image: mountUlapCamp,
       title: "Mount Ulap",
       description:
         "One of the most famous hikes in Benguet is Mt Ulap in Itogon.",
     },
     {
+      id: 2,
       image: calaguesIslandsCamp,
       title: "Calaguas Islands",
       description:
         "A paradise of islands that can rival the white sand beauty of Boracay.",
     },
     {
+      id: 3,
       image: onayBeach,
       title: "Onay Beach",
       description:
         "This is one of the best beach camping sites, beautiful and pristine.",
     },
     {
+      id: 4,
       image: sevenSistersWaterfall,
       title: "Seven Sisters Waterfall",
       description: "The Seven Sisters is the 39th tallest waterfall in Norway.",
     },
     {
+      id: 5,
       image: latikReverside,
       title: "Latik Riverside",
       description:
         "Philippines is one of the most dazzling countries in all of Asia.",
     },
     {
+      id: 6,
       image: buloySprings,
       title: "Buloy Springs",
       description:
@@ -47,13 +54,15 @@ const CampgroundsSection = () => {
   return (
     <div className="mt-10 px-5 max-w-screen-2xl mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-        {cards.map((card, index) => (
-          <CampgroundCard
-            key={index}
-            image={card.image}
-            title={card.title}
-            description={card.description}
-          />
+        {cards.map((card) => (
+          <Link to={`/camp/${card.id}`}>
+            <CampgroundCard
+              key={card.id}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+            />
+          </Link>
         ))}
       </div>
     </div>
