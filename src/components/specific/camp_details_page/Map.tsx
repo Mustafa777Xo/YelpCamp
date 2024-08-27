eff2ee6183e1ad986d61a0244c08f5b80c8cd615
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-27 17:53:43 +0300

    add map component

---
+++ src/components/specific/camp_details_page/Map.tsx
@@ -0,0 +1,12 @@
+import mapImg from "@/assets/images/Map.png";
+const Map = () => {
+  return (
+    <>
+      <div className="container border rounded p-10">
+        <img src={mapImg} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Map;
--- src/pages/CampDetailsPage.tsx
+++ src/pages/CampDetailsPage.tsx
@@ -1,10 +1,11 @@
 import CampDetailsLayout from "@/layouts/CampDetailsLayout";
 import CampCard from "@/components/specific/camp_details_page/CampCard";
+import Map from "@/components/specific/camp_details_page/Map";
 const CampDetailsPage = () => {
   return (
     <>
       <CampDetailsLayout
-        maps="maps"
+        maps={<Map />}
         campCard={<CampCard />}
         commentsSection="comment section here"
       />
