80dd52557216bc4b8cf8c51d21bb18874507ad2f
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-28 19:53:18 +0300

    refactor : links

--- src/components/specific/home_page/AddCampgroundLink.tsx
+++ src/components/specific/home_page/AddCampgroundLink.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const AddCampgroundLink = () => {
   return (
-    <a href="#" className="text-green-500 underline mt-4 block xl:mt-4">
-      Or add your own campground
-    </a>
+    <Link to="/add-camp">
+      <a href="#" className="text-green-500 underline mt-4 block xl:mt-4">
+        Or add your own campground
+      </a>
+    </Link>
   );
 };
 
--- src/layouts/AddCampLayout.tsx
+++ src/layouts/AddCampLayout.tsx
@@ -1,12 +1,16 @@
 import BaseButton from "@/components/common/BaseButton";
 import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 interface AddCampLayoutProps {
   form: React.ReactNode;
 }
 const AddCampLayout: React.FC<AddCampLayoutProps> = ({ form }) => {
   return (
     <div className="container h-screen mx-auto">
-      <BaseButton text="back" btnType="text" icon={FaArrowLeft} />
+      <Link to="/home">
+        <BaseButton text="back" btnType="text" icon={FaArrowLeft} />
+      </Link>
+
       <div className="container flex justify-center">{form}</div>
     </div>
   );
