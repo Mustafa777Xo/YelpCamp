6eb80e16177f373bd2a24b036aac81e0d4d096d1
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-08-28 20:04:10 +0300

    update CampDetails.tsx add back button

--- src/layouts/CampDetailsLayout.tsx
+++ src/layouts/CampDetailsLayout.tsx
@@ -1,3 +1,7 @@
+import BaseButton from "@/components/common/BaseButton";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 interface CampDetailsLayoutProps {
   maps: React.ReactNode;
   campCard: React.ReactNode;
@@ -11,6 +15,9 @@ const CampDetailsLayout: React.FC<CampDetailsLayoutProps> = ({
   return (
     <>
       <div className="container mx-auto py-10">
+        <Link to="/home">
+          <BaseButton text="back" btnType="text" icon={FaArrowLeft} />
+        </Link>
         <div className="colmuns-1 lg:columns-2 lg:gap-5 ">
           <div className="p-5">{maps}</div>
           <div className="mt-16 lg:mt-96 p-5">{campCard}</div>
