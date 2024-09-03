5658e869461e140aec30f37aeaa470386acf99b0
Mustafa777Xo <mustafa777bdo@gmail.com>
2024-09-03 11:13:30 +0300

    add valdition for the sigunp

--- src/components/specific/signup_page/SignupContent.tsx
+++ src/components/specific/signup_page/SignupContent.tsx
@@ -3,8 +3,43 @@ import BaseTextField from "@/components/common/BaseTextfield";
 import { FaArrowLeft } from "react-icons/fa";
 import logo from "@/assets/images/Logo.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SignupContent = () => {
+  const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [password, setPassword] = useState("");
+  const [reTypePassword, setreTypePassword] = useState("");
+
+  const [errors, setErrors] = useState<{
+    email?: string;
+    password?: string;
+    fullName?: string;
+    reTypePassword?: string;
+  }>({});
+
+  const handleSignUp = () => {
+    const newErrors: {
+      email?: string;
+      password?: string;
+      fullName?: string;
+      reTypePassword?: string;
+    } = {};
+
+    if (!email) newErrors.email = "Email is required";
+    if (!fullName) newErrors.fullName = "fullName is required";
+    if (!password) newErrors.password = "Password is required";
+    if (!reTypePassword) newErrors.reTypePassword = "You must retype password";
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      // Proceed with the login logic
+      console.log("Logging in...", { email, password });
+      // Clear errors
+      setErrors({});
+    }
+  };
   return (
     <>
       <div className="container max-w-xl">
@@ -27,23 +62,45 @@ const SignupContent = () => {
               label="Enter your email"
               placeholder="example@gmail.com"
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
             <BaseTextField
               label="Full Name"
               placeholder="ex: john doe"
               type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
             />
+            {errors.fullName && (
+              <p className="text-red-500 text-sm mt-1">{errors.fullName}</p>
+            )}
             <BaseTextField
               label="Password"
               placeholder="Enter your password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
             <BaseTextField
               label="Retype password"
               placeholder="password"
               type="password"
+              value={reTypePassword}
+              onChange={(e) => setreTypePassword(e.target.value)}
             />
-            <BaseButton text="Signup" btnType="filled" />
+            {errors.reTypePassword && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.reTypePassword}
+              </p>
+            )}
+            <BaseButton text="Signup" btnType="filled" onClick={handleSignUp} />
 
             <div className="flex items-center gap-2">
               <p>Have and accont?</p>
